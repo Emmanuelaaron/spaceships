04e9a92ca18f736c3dc40f4c39089da93697558d
mariordgez <mariordgez@hotmail.com>
2021-10-05 13:25:20 -0700

    feat: Fetch mission data from API once per page load

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,3 +1,16 @@
-const Missions = () => <div className="profile">coming soon</div>;
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { bindActionCreators } from 'redux';
+import { loadMissions } from '../redux/missions/missions';
 
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
+  const loadMissionsAction = bindActionCreators(loadMissions, dispatch);
+  useEffect(() => {
+    if (missions.length === 0) loadMissionsAction();
+    return () => null;
+  }, []);
+  return <div className="profile">coming soon</div>;
+};
 export default Missions;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import dragonReducer from './dragons/dragons';
+import missionReducer from './missions/missions';
 
 const reducer = combineReducers({
   dragons: dragonReducer,
+  missions: missionReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(logger, thunk));
