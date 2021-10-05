a3a98ee364439ce0e621c117fb2999e32ebae8e2
mariordgez <mariordgez@hotmail.com>
2021-10-05 13:36:37 -0700

    feat: Missions page renders list of missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -11,6 +11,15 @@ const Missions = () => {
     if (missions.length === 0) loadMissionsAction();
     return () => null;
   }, []);
-  return <div className="profile">coming soon</div>;
+  return (
+    <div className="profile">
+      {missions.map((mission) => (
+        <div key={mission.id}>
+          <h2>{mission.mission_name}</h2>
+          <p>{mission.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Missions;
