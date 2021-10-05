ec79d0a44485175c5b3985a755ee3b5f248b40c3
Enio N. de Souza <enionsouza@gmail.com>
2021-10-05 17:22:47 -0400

    feat: Display Dragons and add styles to Dragons component

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { bindActionCreators } from 'redux';
+import Card from 'react-bootstrap/Card';
 import { loadDragons } from '../redux/dragons/dragons';
 
 const Dragons = () => {
@@ -14,7 +15,23 @@ const Dragons = () => {
   }, []);
 
   return (
-    <div className="profile">coming soon</div>
+    <div className="d-flex flex-column flex-md-row align-items-center mt-5">
+      {dragons.map((dragon) => (
+        <Card
+          bg="light"
+          key={dragon.id}
+          text="dark"
+          style={{ width: '50vw' }}
+          className="m-2 align-self-stretch"
+        >
+          <Card.Img variant="left" src={dragon.flickr_images[0]} />
+          <Card.Header>{dragon.type}</Card.Header>
+          <Card.Body className="d-flex flex-column align-items-center justify-content-center">
+            <Card.Title>{dragon.name}</Card.Title>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
   );
 };
 
