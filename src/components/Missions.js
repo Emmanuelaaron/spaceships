f595f9431e17b900c4c66b22f887f8367017046b
mariordgez <mariordgez@hotmail.com>
2021-10-05 13:55:16 -0700

    feat: Add style to table of missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,6 +1,10 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { bindActionCreators } from 'redux';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
 import { loadMissions } from '../redux/missions/missions';
 
 const Missions = () => {
@@ -12,14 +16,34 @@ const Missions = () => {
     return () => null;
   }, []);
   return (
-    <div className="profile">
-      {missions.map((mission) => (
-        <div key={mission.id}>
-          <h2>{mission.mission_name}</h2>
-          <p>{mission.description}</p>
-        </div>
-      ))}
-    </div>
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <tr key={mission.id}>
+              <td>{mission.mission_name}</td>
+              <td>{mission.description}</td>
+              <td>
+                {' '}
+                <Button variant="secondary">Not a Member</Button>
+                {' '}
+              </td>
+              <td>
+                <Button variant="outline-secondary">Join Mission</Button>
+                {' '}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 };
 export default Missions;
