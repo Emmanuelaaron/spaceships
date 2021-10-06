59c18552c1c562d0ee56dc720b52a57d39c75116
mariordgez <mariordgez@hotmail.com>
2021-10-06 06:02:50 -0700

    chore: Add leave mission action creator and bind to leave btn

--- src/components/JoinLeaveBtn.js
+++ src/components/JoinLeaveBtn.js
@@ -1,7 +1,7 @@
 import Button from 'react-bootstrap/Button';
 import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types'; // eslint-disable-line import/no-extraneous-dependencies
-import { selectMission } from '../redux/missions/missions';
+import { selectMission, leaveMission } from '../redux/missions/missions';
 
 const JoinLeaveBtn = ({ reserved, id }) => {
   const dispatch = useDispatch();
@@ -11,9 +11,15 @@ const JoinLeaveBtn = ({ reserved, id }) => {
     dispatch(selectMission(id));
   };
 
+  const leaveMissionToStore = (e) => {
+    e.preventDefault();
+
+    dispatch(leaveMission(id));
+  };
+
   if (reserved === true) {
     return (
-      <Button onClick={addMissionToStore} variant="outline-secondary">
+      <Button onClick={leaveMissionToStore} variant="outline-secondary">
         Leave Mission
       </Button>
     );
