7d13abc2c5a8c4ac01ca340f721a9f7439314caf
mariordgez <mariordgez@hotmail.com>
2021-10-06 06:55:33 -0700

    feat: Joined missions now appear on myprofile page

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,3 +1,22 @@
-const MyProfile = () => <div className="profile" />;
+import { useSelector } from 'react-redux';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+
+const MyProfile = () => {
+  const missions = useSelector((state) => state.missions);
+  return (
+    <Container>
+      <Card style={{ width: '18rem' }}>
+        <Card.Header>Missions</Card.Header>
+        {missions.map((mission) => (
+          <ListGroup key={mission.mission_id}>
+            {mission.reserved && <ListGroup.Item>{mission.mission_name}</ListGroup.Item>}
+          </ListGroup>
+        ))}
+      </Card>
+    </Container>
+  );
+};
 
 export default MyProfile;
