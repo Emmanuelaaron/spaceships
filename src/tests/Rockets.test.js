28b26dfe7bf69af243be7ee31b126213811d86b4
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-10-08 00:44:39 +0300

    chore: adds test for the Rocket component

---
+++ src/tests/Rockets.test.js
@@ -0,0 +1,18 @@
+import { Provider } from 'react-redux';
+import { render, screen } from '@testing-library/react';
+import store from '../redux/configureStore';
+import Rockets from '../components/Rockets';
+
+describe('Rockets component', () => {
+  describe('Use Jest snapshots to test the Rockets component', () => {
+    it('renders Rockets component as expected', async () => {
+      const RocketsComponent = render(
+        <Provider store={store}>
+          <Rockets />
+        </Provider>,
+      );
+
+      expect(RocketsComponent.asFragment()).toMatchSnapshot();
+    });
+  });
+});
