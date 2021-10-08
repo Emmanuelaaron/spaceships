9be2cf911ba53aafb51387ffc36e8fa297a598cf
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-10-08 19:50:47 +0300

    chore: Implement changes for deployment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
-import './styles/index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
 import App from './App';
 
 ReactDOM.render(
